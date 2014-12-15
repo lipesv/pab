7a0923c6483018df33776bfc0bb1722d01f31542
Fellipe Vieira <fellipe.svieira@gmail.com>
2014-12-15 12:28:28 -0200

    PHP for Absolute Beginners Poll Project Chapter 6 Completed the first part of the book.

--- poll/controllers/poll.php
+++ poll/controllers/poll.php
@@ -2,10 +2,23 @@
 
 // complete code listing for controllers/poll.php
 include_once 'models/Poll.class.php';
-//Only change here: pass PDO object as argument
-$poll = new Poll($db);
+
+// Only change here: pass PDO object as argument
+$poll = new Poll ( $db );
+
+// check if form was submitted
+$isPollSubmitted = isset ( $_POST ['user-input'] );
+
+if ($isPollSubmitted) {
+	
+	// get input received from form
+	$input = $_POST ['user-input'];
+	
+	// ...update model
+	$poll->updatePoll ( $input );
+}
 
 $pollData = $poll->getPollData ();
-$pollView = include_once 'views/poll-html.php';
+$pollAsHTML = include_once 'views/poll-html.php';
 
-return $pollView;
+return $pollAsHTML;
